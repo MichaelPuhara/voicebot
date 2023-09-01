f7f6f7155f41aa2f363952fd8ba4b793c3130fa8
MichaelPuhara <121073103+MichaelPuhara@users.noreply.github.com>
2023-09-01 20:56:34 +1200

    Update app.py

--- app.py
+++ app.py
@@ -29,3 +29,39 @@ if st.button("Test Voice Output", key="voice_button"):
     stream_audio_response("The Korean word for cat is 고양이!")
     stream_audio_response("The Korean word for cat is")
     stream_audio_response("고양이!")
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+user_input_placeholder = st.empty()
+user_input = user_input_placeholder.text_input("You:", value="")
+
+if user_input:
+    # Add the user message to the session_state messages
+    st.session_state.messages.append({"role": "user", "content": user_input})
+
+    # Handle the LLM call
+    with st.chat_message("assistant"):
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+        
+        # Append the LLM response to the session_state messages
+        st.session_state.messages.append(
+            {"role": "assistant", "content": full_response}
+        )
+
+    # Add the messages to the application's frontend display
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
