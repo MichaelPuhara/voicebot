45af5c0e023c0fda781b94132ba12b95e2e077ab
MichaelPuhara <121073103+MichaelPuhara@users.noreply.github.com>
2023-09-02 13:46:13 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 import streamlit as st
-import openai
-from elevenlabs import set_api_key
-from elevenlabs import generate, play
-
-# Set your OPENAI API key
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+from elevenlabs import set_api_key, generate, play
 
 # Set your Eleven Labs API key
+
+def set_api_key():
+    # Get the Eleven Labs API key from the user
+    api_key = st.text_input("Enter your Eleven Labs API key:")
+    
+    # Store the API key as a secret
+    st.secrets["ELEVEN_KEY"] = api_key
+
+def main():
+    st.title("My Streamlit App")
+    
+    # Add a button to set the API key
+    if st.button("Set Eleven Labs API Key"):
+        set_api_key()
+        
+    # Use the API key in your app
+    api_key = st.secrets["ELEVEN_KEY"]
+    
+    # Rest of your Streamlit app logic here
+    # You can use the `api_key` variable to make API requests
+    
+if __name__ == "__main__":
+    main()
+
 ELEVEN_KEY = st.secrets["ELEVEN_KEY"]
 set_api_key(ELEVEN_KEY)
 
@@ -29,39 +48,3 @@ if st.button("Test Voice Output", key="voice_button"):
     stream_audio_response("The Korean word for cat is 고양이!")
     stream_audio_response("The Korean word for cat is")
     stream_audio_response("고양이!")
-
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-user_input_placeholder = st.empty()
-user_input = user_input_placeholder.text_input("You:", value="")
-
-if user_input:
-    # Add the user message to the session_state messages
-    st.session_state.messages.append({"role": "user", "content": user_input})
-
-    # Handle the LLM call
-    with st.chat_message("assistant"):
-        full_response = ""
-        for response in openai.ChatCompletion.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-        
-        # Append the LLM response to the session_state messages
-        st.session_state.messages.append(
-            {"role": "assistant", "content": full_response}
-        )
-
-    # Add the messages to the application's frontend display
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
